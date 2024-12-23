c16c58366aa4cde097674e53fa1d31f27219f809
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-12-23 17:51:33 -0600

    Changes to db and server.js

--- db.js
+++ db.js
@@ -1,19 +1,22 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.message);
+    console.error("Error connecting to the database:", err.message);
     return;
   }
-  console.log('Connected to the Hostinger database!');
+  console.log("Connected to the Hostinger database!");
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on PORT: ${PORT}`);
 });
